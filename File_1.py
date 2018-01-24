09b8b5e60205ba057a48c00194f491da111884b4
HCL-271 <32020828+HCL-271@users.noreply.github.com>
2018-01-25 00:04:36 +0300

    Update File_1.py

--- File_1.py
+++ File_1.py
@@ -1,7 +1,8 @@
+
 H = 40.0
 g = -9.81
-t = 0.07
-r=0.1
+t = 0.03
+r=0.5
 y=0
 x=1
 uy=2
@@ -32,26 +33,31 @@ class Molekula:
 
 
 
-for k in range(1, 10):
-   for i in range (len(Particles[0])):
-       j=2
-       print(i,j)
-       for j in range(len(Particles[0])):
-        if pow(((Particles[y][i]-Particles[y][j]) **2 + (Particles[x][i]-Particles[x][j])**2),0.5)<=r:
-            Particles[uy][i] *= -1
-            Particles[ux][i] *= -1
-            Particles[uy][j] *= -1
-            Particles[ux][j] *= -1
-            print("Kick")
-
-        print(Particles[y][j])
-        print(Particles[y][i])
-
-        Particles[uy][i] += g * t
-        Particles[y][i] += Particles[uy][i] * t + (g * t ** 2) / 2
-        Particles[x][i] += ux * t
-        if Particles[y][i] <= 0:
-            if Particles[uy][i] < 0:
-                Particles[uy][i] = -0.6 * Particles[uy][i]
-   #print("%.2f" % Particles[y][j],s,"%.2f" % Particles[uy][j],s,"%.2f" % t1)
-
+for k in range(1, 100):
+   i=0
+   j=0
+   for i in range (len(Particles[0])-1):
+        j=i+1
+        if j < 2:
+            print(i, j)
+            for j in range(len(Particles[0]) - 1):
+                if pow(((Particles[y][0] - Particles[y][1]) ** 2 + (Particles[x][0] - Particles[x][1]) ** 2), 0.5) <= r:
+                    Particles[uy][i] *= -1
+                    Particles[ux][i] *= -1
+                    Particles[uy][j] *= -1
+                    Particles[ux][j] *= -1
+                    print("Kick")
+        Particles[uy][0] += g * t
+        Particles[y][0] += Particles[uy][0] * t + (g * t ** 2) / 2
+        Particles[x][0] += Particles[ux][0] * t
+        if Particles[y][0] <= 0:
+            if Particles[uy][0] < 0:
+                Particles[uy][0] = -0.6 * Particles[uy][0]
+        Particles[uy][1] += g * t
+        Particles[y][1] += Particles[uy][1] * t + (g * t ** 2) / 2
+        Particles[x][1] += Particles[ux][1] * t
+        if Particles[y][1] <= 0:
+            if Particles[uy][1] < 0:
+                Particles[uy][1] = -0.6 * Particles[uy][1]
+   print("%.2f" % Particles[y][1],s,"%.2f" % Particles[uy][1],s,"%.2f" % t1)
+   print("%.2f" % Particles[y][0], s, "%.2f" % Particles[uy][0], s, "%.2f" % t1)
