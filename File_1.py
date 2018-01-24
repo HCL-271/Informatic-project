bffc63eb68373ca8efa9ae9250b93d6906ca4916
HCL-271 <32020828+HCL-271@users.noreply.github.com>
2018-01-25 00:17:44 +0300

    Update File_1.py

--- File_1.py
+++ File_1.py
@@ -1,8 +1,8 @@
 
 H = 40.0
 g = -9.81
-t = 0.03
-r=0.5
+t = 0.07
+r=1
 y=0
 x=1
 uy=2
@@ -11,17 +11,19 @@ t1 = 0.07
 s="   "
 mol = []
 Particles = [[H,0.5*H],[0,0],[0,0],[0,0]]
-print(Particles[y][1])
+print(len(Particles[y]))
 class Molekula:
     def __init__(self,i,j):
         self.i ,self.j=i,j
         mol.append(self)
 
     def kick(self):
-        Particles[uy][i] *= -1
-        Particles[ux][i] *= -1
-        Particles[uy][j] *= -1
-        Particles[ux][j] *= -1
+        a= Particles[uy][i]
+        b =Particles[ux][i]
+        Particles[uy][i] = Particles[uy][j]
+        Particles[ux][i] = Particles[ux][j]
+        Particles[uy][j] = a
+        Particles[ux][j] = b
     def move(self):
         Particles[uy][i] += g * t
         Particles[y][i] += Particles[uy][i] * t + (g * t ** 2) / 2
@@ -33,19 +35,25 @@ class Molekula:
 
 
 
-for k in range(1, 100):
+for k in range(1, 50):
    i=0
    j=0
-   for i in range (len(Particles[0])-1):
+   for i in range (len(Particles[0])):
         j=i+1
-        if j < 2:
-            print(i, j)
-            for j in range(len(Particles[0]) - 1):
-                if pow(((Particles[y][0] - Particles[y][1]) ** 2 + (Particles[x][0] - Particles[x][1]) ** 2), 0.5) <= r:
-                    Particles[uy][i] *= -1
-                    Particles[ux][i] *= -1
-                    Particles[uy][j] *= -1
-                    Particles[ux][j] *= -1
+
+
+
+        for j in range(len(Particles[0])):
+            if ((j < len(Particles[y])) and (i != j)):
+                 print(i, j)
+                 if pow(((Particles[y][0] - Particles[y][1]) ** 2 + (Particles[x][0] - Particles[x][1]) ** 2), 0.5) <= r:
+                    a= Particles[uy][0]
+                    b =Particles[ux][0]
+                    Particles[uy][0] = Particles[uy][1]
+                    Particles[ux][0] = Particles[ux][1]
+                    Particles[uy][1] = a
+                    Particles[ux][1] = b
+
                     print("Kick")
         Particles[uy][0] += g * t
         Particles[y][0] += Particles[uy][0] * t + (g * t ** 2) / 2
